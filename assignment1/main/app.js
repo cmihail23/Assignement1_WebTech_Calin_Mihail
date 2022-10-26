3d75e827a6808fde8f28eef150408cdd13e39969
Mihail <mihailcalin.2002@yahoo.com>
2022-10-26 23:36:40 +0300

    solve asignment

--- assignment1/main/app.js
+++ assignment1/main/app.js
@@ -1,6 +1,35 @@
 function hash(input, dictionary) {
-	//  TODO: implementați funcția
-	
+	if (typeof input != "string") {
+		throw new Error("Input should be a string");
+	}
+	for (let x of dictionary) {
+		if (typeof x != "string") {
+			throw new Error("Invalid dictionary format")
+		}
+	};
+	let result = [];
+	input.split(' ').forEach((element) => {
+		let word = '';
+		let found = false;
+		for (let comparer of dictionary) {
+			if (comparer.toLowerCase() === element.toLowerCase()) {
+				word += element.slice(0, 1);
+				element.slice(1, -1).split('').forEach(
+					(letter) => {
+						word += '#';
+					}
+				);
+				word += element.slice(-1);
+				found = true;
+				break;
+			} 
+		}
+		if(!found){
+			word = element;
+		}
+		result.push(word)
+	})
+	return result.join(' ');
 }
 
 const app = {
@@ -8,3 +37,7 @@ const app = {
 };
 
 module.exports = app;
+
+
+let res = hash("The dog is walking fast", ['walking', 'fast', 'dog'])
+console.log(res)
